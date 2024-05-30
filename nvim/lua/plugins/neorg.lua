d5354ff060d9fb3c71d0c5dd956eb3c2bbab5aca
Geoff Cadien <gcadien@gmail.com>
2024-05-30 14:24:17 -0700

    Updates.
    - Remove nvim orgmode.  Replace with neorg.
    - Update neorg

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -1,24 +1,31 @@
 return {
-  "nvim-neorg/neorg",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  build = ":Neorg sync-parsers",
-  -- tag = "*",
-  lazy = true, -- enable lazy load
-  ft = "norg", -- lazy load on file type
-  cmd = "Neorg", -- lazy load on command
-  config = function()
-    require("neorg").setup {
-      load = {
-        ["core.defaults"] = {}, -- Loads default behaviour
-        ["core.concealer"] = {}, -- Adds pretty icons to your documents
-        ["core.dirman"] = { -- Manages Neorg workspaces
-          config = {
-            workspaces = {
-              notes = "~/notes",
-            },
-          },
-        },
-      },
-    }
-  end,
+    "nvim-neorg/neorg",
+    dependencies = { "luarocks.nvim" },
+    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    version = "*", -- Pin Neorg to the latest stable release
+    config = true,
 }
+--return {
+--  "nvim-neorg/neorg",
+--  dependencies = { "nvim-lua/plenary.nvim" },
+--  build = ":Neorg sync-parsers",
+--  -- tag = "*",
+--  lazy = true, -- enable lazy load
+--  ft = "norg", -- lazy load on file type
+--  cmd = "Neorg", -- lazy load on command
+--  config = function()
+--    require("neorg").setup {
+--      load = {
+--        ["core.defaults"] = {}, -- Loads default behaviour
+--        ["core.concealer"] = {}, -- Adds pretty icons to your documents
+--        ["core.dirman"] = { -- Manages Neorg workspaces
+--          config = {
+--            workspaces = {
+--              notes = "~/notes",
+--            },
+--          },
+--        },
+--      },
+--    }
+--  end,
+--}
