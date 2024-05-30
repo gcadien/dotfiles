bb2e95a93fc9940a491b924fbbb54e60136be60b
Geoff Cadien <gcadien@gmail.com>
2024-05-30 15:08:15 -0700

    Added toggleterm and keymappings

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -5,27 +5,4 @@ return {
     version = "*", -- Pin Neorg to the latest stable release
     config = true,
 }
---return {
---  "nvim-neorg/neorg",
---  dependencies = { "nvim-lua/plenary.nvim" },
---  build = ":Neorg sync-parsers",
---  -- tag = "*",
---  lazy = true, -- enable lazy load
---  ft = "norg", -- lazy load on file type
---  cmd = "Neorg", -- lazy load on command
---  config = function()
---    require("neorg").setup {
---      load = {
---        ["core.defaults"] = {}, -- Loads default behaviour
---        ["core.concealer"] = {}, -- Adds pretty icons to your documents
---        ["core.dirman"] = { -- Manages Neorg workspaces
---          config = {
---            workspaces = {
---              notes = "~/notes",
---            },
---          },
---        },
---      },
---    }
---  end,
---}
+
